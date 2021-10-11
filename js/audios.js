87649a061875c4a945d652488c8bb4de09751b9b
SisneLanguages <92319767+SisneLanguages@users.noreply.github.com>
2021-10-11 16:39:55 +0200

    Add files via upload

---
+++ js/audios.js
@@ -0,0 +1,34 @@
+// Audio  0
+var bleep = new Audio();
+bleep.src = 'bleep.mp3';
+//Audio 1
+var one =new Audio();
+one.src='one.mp3'
+//Audio 2
+var two =new Audio();
+two.src='two.mp3'
+//Audio 3
+var three =new Audio();
+three.src='three.mp3'
+//Audio 4 
+var four =new Audio();
+four.src='four.mp3'
+//Audio 5 
+var five =new Audio();
+five.src='five.mp3'
+//Audio 6 
+var six =new Audio();
+six.src='six.mp3'
+//Audio 7 
+var seven =new Audio();
+seven.src='seven.mp3'
+//Audio 8 
+var eight =new Audio();
+eight.src='eight.mp3'
+//Audio 9 
+var nine =new Audio();
+nine.src='nine.mp3'
+//Audio 10 
+var ten =new Audio();
+ten.src='ten.mp3'
+
